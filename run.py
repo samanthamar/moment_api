ad522154dd8c66537c6be816578b868c3b5b77ca
Quin Millard <midnitefreddy@hotmail.com>
2020-02-17 19:50:43 -0500

    update run

--- run.py
+++ run.py
@@ -1,17 +1,17 @@
 from flask import Flask
 
 def create_app(config_filename):
-    app = Flask(__name__)
-    app.config.from_object(config_filename)
+    application = Flask(__name__)
+    application.config.from_object(config_filename)
 
     from app import api_bp
-    app.register_blueprint(api_bp, url_prefix='/api')
+    application.register_blueprint(api_bp, url_prefix='/api')
 
     from models.Model import db
-    db.init_app(app)
+    db.init_app(application)
 
-    return app
+    return application
 
 if __name__ == "__main__":
-    app = create_app("config")
-    app.run(debug=True)
+    application = create_app("config")
+    application.run(debug=True)
