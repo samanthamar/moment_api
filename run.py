782654090f275e82e6e3038b951b6ce2537956f3
Quin Millard <midnitefreddy@hotmail.com>
2020-02-21 17:48:56 -0500

    update for eb

--- run.py
+++ run.py
@@ -1,17 +1,14 @@
 from flask import Flask
 
-def create_app(config_filename):
-    application = Flask(__name__)
-    application.config.from_object(config_filename)
+application = Flask(_name_)
+
+if __name__ == "__main__":
+    CORS(application)
+    application.config.from_object("config")
 
     from app import api_bp
     application.register_blueprint(api_bp, url_prefix='/api')
 
     from models.Model import db
     db.init_app(application)
-
-    return application
-
-if __name__ == "__main__":
-    application = create_app("config")
     application.run(debug=True)
