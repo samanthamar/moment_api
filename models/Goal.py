3c9fdf1c990e5745deb8fa1041e11de3017ab1d7
samanthamar <samantha.mar017@gmail.com>
2020-02-29 15:03:49 -0500

    add goal completion timestamp column to model

--- models/Goal.py
+++ models/Goal.py
@@ -10,13 +10,15 @@ class Goal(db.Model):
     timestamp = db.Column(db.TIMESTAMP, default=datetime.now(), nullable=False)
     status = db.Column(db.String(20), default='incomplete')
     category = db.Column(db.String(20))
+    completion_time = db.Column(db.TIMESTAMP) 
 
-    def __init__(self, user_id, goal, tags, category, status):
+    def __init__(self, user_id, goal, tags, category, status, completion_time=None):
         self.user_id = user_id 
         self.goal = goal
         self.tags = tags
         self.category = category
         self.status = status
+        completion_time = completion_time
 
     def __repr__(self):
         return '<Goal %r>' % (self.goal)
@@ -25,4 +27,4 @@ class GoalSchema(ma.Schema):
     class Meta:
         fields = ("id", "user_id", 
                     "goal", "tags", 
-                    "timestamp", "status", "category")
+                    "timestamp", "status", "category", "completion_time")
